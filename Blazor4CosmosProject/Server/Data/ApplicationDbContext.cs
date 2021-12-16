57bea6e7e83d95f6ce088eab5cf9165f0b8ec68f
Jaaron Sanderson <jaaron@krplastic.com>
2021-12-16 10:02:23 -0600

    Add project files.

---
+++ Blazor4CosmosProject/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Blazor4CosmosProject.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Blazor4CosmosProject.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Blazor4CosmosProject/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blazor4CosmosProject.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
